805c7e9eda8b6ffde90c90a31b5c4cc5e754bcb9
MadMonkDev <aviralkmdubey@gmail.com>
2025-05-29 22:26:28 +0530

    some opt

--- app/src/dex.h
+++ app/src/dex.h
@@ -1,12 +1,20 @@
 
 /* All Header files */
-#include <linux/limits.h>
+#include <cstddef>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
-#include <limits.h>
 
-void dex_func(const char *path, int depth, long *size);
+typedef struct size_count {
+  size_t dir;
+  size_t files;
+} count_t ;
+
+typedef struct entry_dir {
+  char *dir_name;
+  bool is_dir;
+  struct entry_dir *next;
+} entry_t ;
+
+
