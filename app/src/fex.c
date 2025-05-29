74e946a31f96b9e0e051ce06b4f44557daaeeae8
MadMonkDev <aviralkmdubey@gmail.com>
2025-05-29 10:20:14 +0530

    initialised the project

---
+++ app/src/fex.c
@@ -0,0 +1,4 @@
+
+#include "fex.h"
+
+
---
+++ app/src/fex.h
@@ -0,0 +1,11 @@
+
+/* All Header files */
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <limits.h>
+
+
