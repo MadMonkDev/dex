5c4891b8076db15c13915ff4d26d04c8a53e2af7
MadMonkDev <aviralkmdubey@gmail.com>
2025-06-04 19:22:17 +0530

    added uninstall support

--- cmake_uninstall.cmake
+++ cmake_uninstall.cmake
@@ -1,7 +1,7 @@
-
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
     set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 
+message(STATUS "Uninstalling from ${CMAKE_INSTALL_PREFIX}")
+
 file(REMOVE "${CMAKE_INSTALL_PREFIX}/bin/dex")
-message(STATUS "Uninstalled: ${CMAKE_INSTALL_PREFIX}/bin/dex")
